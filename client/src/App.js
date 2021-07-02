15583bc7b2f439792dea13bb5db521378c53d113
Hritam Dutta <hritamloyola10@gmail.com>
2021-07-03 00:35:28 +0530

    React App Files Changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import UserForm from './components/UserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="col-lg-8 mx-auto text-center mt-5">
+          <img srcSet="beelogo.jpg" alt="Bee Logo"></img><h1><b>Resume!</b></h1>
+          <p className="lead">Please provide clear and accurate information.</p>
+          <hr />
+        </div>  
+        <UserForm/>
+      </div>  
+    );
+  }
 }
 
 export default App;
