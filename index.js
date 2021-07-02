06686aa27b5652280f3e2d253e474fa8bf6adab1
Hritam Dutta <hritamloyola10@gmail.com>
2021-07-02 11:05:25 +0530

    Main File Set

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+const path = require('path');
+
+const pdfTemplate = require('./resume_template');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//PDF generation & fetching 
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('Resume.pdf', (err) => {
+        if(err){
+            res.send(Promise.reject());
+            console.log(err);
+        }
+
+        res.send(Promise.resolve());
+        console.log('Success !');
+    });
+});
+
+// Sending generated pdf 
+app.get('/fetch-pdf', (req,res) => {
+    res.sendFile(`${__dirname}/My Resume.pdf`);
+});
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Server started on port ${port} ...`));
