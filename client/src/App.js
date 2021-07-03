77a6411bb80a909596b4ad45669ba93885b5dd1a
Hritam Dutta <hritamloyola10@gmail.com>
2021-07-04 02:16:51 +0530

    Heading Made Responsive

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="col-lg-8 mx-auto text-center mt-5">
-          <h1><img src={logo} alt="Bee Logo" style={{verticalAlign: "baseline", width: "100px"}}/>
-            <b style={{verticalAlign: "bottom", fontWeight: "780", fontSize: "70px", color: "black"}}>Resume</b>
-          </h1>
+        <div className="col-lg-8 mx-auto text-center mt-4 head">
+          <h1><img src={logo} alt="Bee Logo"/><b>Resume</b></h1>
           <p className="lead">Get going by building an exciting resume !</p>
           <hr />
         </div>  
